a0c4ea76dbf5ad44a51fce452d4ea9077f772cd2
serweryn617 <seweryn.r.1999@gmail.com>
2025-03-30 12:00:53 +0200

    Adjust colors; add which-key

--- lua/joyous-wilds/palette.lua
+++ lua/joyous-wilds/palette.lua
@@ -2,23 +2,22 @@ local palette = {
   fg = "#cccccc",
   bg = "#181818",
 
-  blue_1 = "#9cdcfe",
-  blue_2 = "#4fc1ff",
-  blue_3 = "#569cd6",
-  blue_4 = "#264f78",
+  blue_1 = "#9ad6fe",
+  blue_2 = "#3db1ff",
+  blue_3 = "#509fd3",
+  blue_4 = "#255474",
 
-  green_1 = "#b5cea8",
-  green_2 = "#6a9955",
+  green_1 = "#abcda8",
+  green_2 = "#5b9654",
 
-  red = "#d16969",
-  turquoise = "#4ec9b0",
-  coral = "#ce9178",
-  straw = "#dcdcaa",
-  ecru = "#d7ba7d",
-  mustard = "#ffd700",
-  pink = "#c586c0",
-  ochre = "#9e6a03",
-  azure = "#40a6ff",
+  red = "#d06771",
+  turquoise = "#4cc8bb",
+  coral = "#cc9875",
+  straw = "#dbd6a9",
+  ecru = "#d5cc7b",
+  mustard = "#ffe600",
+  pink = "#bd83c3",
+  ochre = "#9b8c03",
 
   white = "#ffffff",
   gray_1 = "#c8c8c8",
@@ -27,6 +26,20 @@ local palette = {
   gray_4 = "#6b6b6b",
   gray_5 = "#616161",
   gray_6 = "#3c3c3c",
+
+  jw_green_1 = "#00cc52",
+  jw_green_2 = "#00b348",
+  jw_green_3 = "#009a3e",
+  jw_green_4 = "#008134",
+  jw_green_5 = "#00682a",
+  jw_green_6 = "#004f20",
+
+  jw_pink_1 = "#d849ff",
+  jw_pink_2 = "#c23ee6",
+  jw_pink_3 = "#ac33cd",
+  jw_pink_4 = "#9628b4",
+  jw_pink_5 = "#801d9b",
+  jw_pink_6 = "#6a1282",
 }
 
 return palette
